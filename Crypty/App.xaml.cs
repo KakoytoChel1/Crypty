afaacc9080612f880363ec233aaaf705c48a9d20
Иван Русин <VanyaRusin2402@gmail.com>
2026-01-07 22:03:38 +0200

    Initial commit

---
+++ Crypty/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace Crypty
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        override protected void OnStartup(StartupEventArgs e)
+        {
+            // Service initialization
+            IntializeServices();
+
+            // Main window initialization
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        private void IntializeServices()
+        {
+
+        }
+    }
+
+}
